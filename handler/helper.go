108cac0ae2a06bb98167e95a7fd259ecaa08ed8f
Morpheus0x <morpheus0x@pm.me>
2023-11-02 16:45:48 +0100

    add IDNA domain support

--- handler/helper.go
+++ handler/helper.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
-	"regexp"
+	"strings"
+
+	"golang.org/x/net/idna"
 )
 
 func validateEmail(email string) (string, error) {
@@ -10,12 +13,20 @@ func validateEmail(email string) (string, error) {
 		return "", fmt.Errorf("emailaddress parameter is missing")
 	}
 
-	// Regular expression to check if the email format is valid
-	// Note: Email regex validation is complex and this simple regex does not cover all cases.
-	re := regexp.MustCompile(`^.+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$`)
-	if !re.MatchString(email) {
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 || len(parts[0]) < 1 || len(parts[1]) < 1 {
 		return "", fmt.Errorf("invalid email format")
 	}
 
-	return email, nil
+	// Convert domain part using IDNA punycode translation
+	convertedDomain, err := idna.ToASCII(parts[1])
+	if err != nil {
+		return "", fmt.Errorf("invalid domain: %v", err)
+	}
+
+	if strings.Count(convertedDomain, ".") < 1 {
+		return "", errors.New("domain must contain at least one period")
+	}
+
+	return parts[0] + "@" + convertedDomain, nil
 }
