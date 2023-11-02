084dc94ac4f87a36a4c46d41c3ff5a08bc2b4ae6
Morpheus0x <morpheus0x@pm.me>
2023-11-03 00:48:21 +0100

    fix default vars and viper pflag integration (not perfect)

--- cli/setup.go
+++ cli/setup.go
@@ -1,22 +1,28 @@
 package cli
 
 import (
-	"fmt"
-
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 const DEFAULT_BIND_IP = "127.0.0.1"
-const DEFAULT_PORT = 8080
+const DEFAULT_PORT = "8080"
+
+var bind = ""
+var port = ""
 
 func Setup() {
 	viper.SetDefault("port", DEFAULT_PORT)
 	viper.SetDefault("bind", DEFAULT_PORT)
 
-	pflag.StringP("port", "p", fmt.Sprintf("%d", DEFAULT_PORT), "Port to run the Autoconfig API on")
-	pflag.StringP("bind", "b", DEFAULT_BIND_IP, "IP to run the Autoconfig API on")
-	pflag.Parse()
+	pflag.StringVarP(&port, "port", "p", DEFAULT_PORT, "Port to run the Autoconfig API on")
+	viper.Set("port", port)
+	pflag.StringVarP(&bind, "bind", "b", DEFAULT_BIND_IP, "IP to run the Autoconfig API on")
+	viper.Set("bind", bind)
+	// pflag.Lookup("port").NoOptDefVal = DEFAULT_PORT
+	// pflag.Lookup("bind").NoOptDefVal = DEFAULT_BIND_IP
 
-	viper.BindPFlags(pflag.CommandLine)
+	// pflag.Parse()
+
+	// viper.BindPFlags(pflag.CommandLine)
 }
