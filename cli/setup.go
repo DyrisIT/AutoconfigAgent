a06a5ac4e3387c43aa7c8e720575ceae3ef46768
Morpheus0x <morpheus0x@pm.me>
2023-11-03 01:06:34 +0100

    fix arg parsing with pflag and viper

--- cli/setup.go
+++ cli/setup.go
@@ -8,21 +8,13 @@ import (
 const DEFAULT_BIND_IP = "127.0.0.1"
 const DEFAULT_PORT = "8080"
 
-var bind = ""
-var port = ""
-
 func Setup() {
+	pflag.StringP("bind", "b", DEFAULT_BIND_IP, "IP to run the Autoconfig API on")
+	pflag.StringP("port", "p", DEFAULT_PORT, "Port to run the Autoconfig API on")
+	pflag.Parse()
+
+	viper.BindPFlags(pflag.CommandLine)
+	viper.SetDefault("bind", DEFAULT_BIND_IP)
 	viper.SetDefault("port", DEFAULT_PORT)
-	viper.SetDefault("bind", DEFAULT_PORT)
 
-	pflag.StringVarP(&port, "port", "p", DEFAULT_PORT, "Port to run the Autoconfig API on")
-	viper.Set("port", port)
-	pflag.StringVarP(&bind, "bind", "b", DEFAULT_BIND_IP, "IP to run the Autoconfig API on")
-	viper.Set("bind", bind)
-	// pflag.Lookup("port").NoOptDefVal = DEFAULT_PORT
-	// pflag.Lookup("bind").NoOptDefVal = DEFAULT_BIND_IP
-
-	// pflag.Parse()
-
-	// viper.BindPFlags(pflag.CommandLine)
 }
